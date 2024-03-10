d11085c7cba6d75ef673d573ac08196572a65793
Guy-Jeef TUMBA <guyjeef16@gmail.com>
2024-03-10 18:20:32 +0000

    add links to course home page using js

---
+++ scripts/links.js
@@ -0,0 +1,46 @@
+document.addEventListener("DOMContentLoaded", function() {
+    
+    const baseURL =  "" ;
+    const linksURL =  "data/links.json" ;
+
+    const ul = document.querySelector('#weeks-content');
+
+    async function getLinks() {
+        try {
+            const response = await fetch (linksURL); 
+            if (response.ok) {
+                const data = await response.json (); 
+                displayLinks (data.weeks);
+                console.log(data.weeks)
+            }else {
+                throw Error (await response.text());
+            } 
+        }catch (error) {
+            console.log(error);
+        }   
+    }
+
+
+    function displayLinks(weeks) { 
+        weeks.forEach(week => {
+            
+            let weekLi = document.createElement('li');
+            weekLi.textContent = `${week.week}: `
+            ul.appendChild(weekLi);
+                week.links.forEach((activity, index) => {
+                    let aActivity = document.createElement('a');
+                    aActivity.textContent = `${activity.title} `;
+                    aActivity.setAttribute('href',`${activity.url} `);
+                    weekLi.append(aActivity);
+                    if (index < week.links.length - 1) {
+                        let separator = document.createElement('span');
+                        separator.textContent = "|";
+                        weekLi.appendChild(separator);
+                    }
+                });
+        });
+    }
+
+    getLinks();
+
+});
