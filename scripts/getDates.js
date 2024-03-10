f58d0a2af8aebb8a4de956a0021026751250f3ef
Guy-Jeef TUMBA <guyjeef16@gmail.com>
2024-03-10 22:52:02 +0000

    W02: Team Activity - Home Page Enhancement

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -9,3 +9,26 @@ lastUpdated.textContent = `Last Updated : ${document.lastModified}`;
 
 footer.appendChild(yearParagraph);
 footer.appendChild(lastUpdated);
+
+const mainnav = document.querySelector('nav')
+const hambutton = document.querySelector('#hamburger');
+const navbar = document.querySelector('nav');
+
+hambutton.addEventListener('click', () => {
+	mainnav.classList.toggle('show');
+	hambutton.classList.toggle('show');
+    navbar.classList.toggle('noDisplay');
+});
+
+const modeButton = document.querySelector("#mode");
+const html = document.querySelector("html");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("🌙")) {
+        html.classList.toggle('dark');
+		modeButton.textContent = "☀️";
+	} else {
+		html.classList.toggle('dark');
+		modeButton.textContent = "🌙";
+	}
+});
